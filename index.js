1b482a7cd76efb5ffa2ba828a07a90bb892e6cf8
kennytobiloba <olaatunbikehinde@gmail.com>
2025-05-08 13:35:58 +0100

    website hosted

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ dotenv.config();
 
 const app = express();
 const port = 3000;
-app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173/', 
+  credentials: true
+}));
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
